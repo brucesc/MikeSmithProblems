ea04b37531511d12cc8444a4868fbac3429d418f
Steven Bruce <Brucesc726@gmail.com>
2018-02-20 20:45:38 -0500

    First solution to the looping section, EvenNumbers

---
+++ EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("evenNumbers(4,13) returns: " + evenNumbers(4, 13));
+            Console.WriteLine("evenNumbers(3,10) returns: " + evenNumbers(3, 10));
+            Console.WriteLine("evenNumbers(8,21) returns: " + evenNumbers(8, 21));
+
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+        static string evenNumbers(int minNumber, int maxNumber)
+        {
+            string someString = "";
+            for (int i = minNumber; i <= maxNumber; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    someString += ", " + i;
+                }
+
+            }
+            return someString;
+        }
+    }
+}
